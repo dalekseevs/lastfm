9f765f5c35fe49b197b149d01f3fdc446c74b323
Dmitrijs.Aleksejevs <dmitrijsaleksejevs@digitecgalaxus.ch>
2023-04-21 13:08:04 +0200

    uncomment

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ mkdir -p ~/lastfm-analyzer-data/output
 echo "Building Docker image..."
 docker build -t lastfm-analyzer .
 
-#echo "Downloading data files..."
-#wget -O - http://mtg.upf.edu/static/datasets/last.fm/lastfm-dataset-1K.tar.gz | tar xz -C ~/lastfm-analyzer-data/input
+echo "Downloading data files..."
+wget -O - http://mtg.upf.edu/static/datasets/last.fm/lastfm-dataset-1K.tar.gz | tar xz -C ~/lastfm-analyzer-data/input
 
 echo "Starting Spark Docker container and running analysis..."
 docker run\
